a38fada1779d7a583f5d92bc805813b46c672868
Asher <asher_3@hotmail.co.uk>
2020-05-11 22:34:52 +0100

    added contact form with animation slider through CSS and JS

--- js/script.js
+++ js/script.js
@@ -24,3 +24,17 @@ const navSlide = () => {
 
 navSlide();
 // END OF NAVBAR ANIMATION
+
+// CONTACT FORM TRANSITION
+const signUpButton = document.getElementById("signUp");
+const signInButton = document.getElementById("signIn");
+const container = document.getElementById("container");
+
+signUpButton.addEventListener("click", () => {
+  container.classList.add("right-panel-active");
+});
+
+signInButton.addEventListener("click", () => {
+  container.classList.remove("right-panel-active");
+});
+// END OF TRANSITION
