3b3f4d108b3c5b2962491186b7741202ec559d7a
Asher <asher_3@hotmail.co.uk>
2020-05-11 15:54:38 +0100

    added carousel in clients section and adding form for contact section

---
+++ js/slider.js
@@ -0,0 +1,19 @@
+// CAROUSEL SLIDER
+$(".clients-owl").owlCarousel({
+  margin: 20,
+  loop: true,
+  autoplay: true,
+  autoplayTimeout: 2000,
+  autoplayHoverPause: true,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 2,
+    },
+    1000: {
+      items: 3,
+    },
+  },
+});
